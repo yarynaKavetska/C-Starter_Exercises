aeb95810ccc488c831a3b331b8ddfcd588371eea
DESKTOP-64DO94L\Yaryna <yaryna.kavetska@gmail.com>
2017-04-04 13:53:39 +0300

    created 2 variables of double and decimal and printed it on console

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.141592653d;
+            decimal e = 2.7182818284590452m;
+            Console.WriteLine(pi);
+            Console.WriteLine(e);
+            Console.ReadKey();
+        }
+    }
+}
