0dbf37c29a334b6a501045b2d046ccda732fd5a8
DESKTOP-64DO94L\Yaryna <yaryna.kavetska@gmail.com>
2017-04-04 13:47:58 +0300

    created additional task 2

---
+++ AdditionalTask2/AdditionalTask2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdditionalTask2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("	- 2, 147, 483, 648.. 2, 147, 483, 647");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("-32,768 .. 32,767");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("- 9, 223, 372, 036, 854, 775, 808.. 9, 223, 372, 036, 854, 775, 807");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("0.. 4, 294, 967, 295");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("	0.. 65, 535");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("	0.. 18, 446, 744, 073, 709, 551, 615");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("0.. 255");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("-128 .. 127");
+        }
+    }
+}
